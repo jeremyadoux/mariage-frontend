5425e47b804ee281e9137925f833d49305a41b70
root <root@vps251700.ovh.net>
2016-03-05 07:50:09 +0100

    passage en full 777 pour pas perdre de temps pour le moment

--- app/master/app.js
+++ app/master/app.js
@@ -1,6 +1,6 @@
 var module = angular.module('app', ['ui.router', 'ngSanitize', 'lbServices', 'ckeditor', 'angularFileUpload', 'uiGmapgoogle-maps', 'timer']).config(function(LoopBackResourceProvider) {
     // Change the URL where to access the LoopBack REST API server
-    LoopBackResourceProvider.setUrlBase('http://192.168.100.100:443/api');
+    LoopBackResourceProvider.setUrlBase('http://api.zarazetti.fr/api');
 });
 
 
